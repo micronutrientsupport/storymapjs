8c61619c491e2a0f4c2c1cd3a0bc614ecf3ecbe3
Daniel Warren - BGS <danwar@bgs.ac.uk>
2021-08-25 11:47:53 +0100

    feat: working with error debug in specific component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+// declare function hello(): any;
+declare const KLStoryMap: any;
 
+function triggerStory() {
+  console.debug('call');
+  var storymap_options = {
+    width: 600,                // required for embed tool; width of StoryMap
+    height: 600,               // required for embed tool; height of StoryMap
+    font_css: 'Calibri',              // optional; font set
+    calculate_zoom: true,              // optional; defaults to true.
+    storymap: {
+      language: 'en',          // required; two-letter ISO language code
+      map_type: 'stamen:watercolor',          // required
+      map_as_image: true,       // required
+      // map_subdomains: string,    // optional
+      slides: [{
+        type: "overview",      // optional; if present must be set to "overview"
+        // location: {            // required for all slides except "overview" slide
+        //   lat: decimal,      // latitude of point on map
+        //   lon: decimal       // longitude of point on map
+        // },
+        text: {                // optional if media present
+          headline: 'TEST',
+          text: 'TEST'       // may contain HTML markup
+        },
+        media: {               // optional if text present
+          url: 'https://miro.medium.com/max/10368/1*1XPSskRR5Mbe3X01BmV3zw.jpeg',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      }]           // required; array of slide objects (see below)
+    }
+  };
+
+  var storymap = new KLStoryMap.StoryMap('mapdiv', storymap_options);
+  window.onresize = function () {
+    storymap.updateDisplay(); // this isn't automatic
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,48 +45,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   // storymap_data can be an URL or a Javascript object
-  public storymap_data = '/static/demo/demo.json';
   public title = 'storymap';
-  public storymap_options = {};
+
 
   ngOnInit() {
-    const storymap_data = 'https://uploads.knightlab.com/storymapjs/8ae5bf425ed73e388274fbce519d1e43/maps-collage/index.html';
+    triggerStory();
+    // fetch("./story.html")
+    //   .then(response => {
+    //     return response.text()
+    //   })
+    //   .then(data => {
+    //     if (null != data) {
+    //       console.debug('call');
+    //      this.element = document.querySelector('body')?.innerHTML;
+    //       this.element = data;
+    //     }
 
-    // certain settings must be passed within a separate options object
-    const storymap_options = {
-      width: 600,                // required for embed tool; width of StoryMap
-      height: 600,               // required for embed tool; height of StoryMap
-      font_css: 'Calibri',              // optional; font set
-      calculate_zoom: true,              // optional; defaults to true.
-      storymap: {
-        language: 'en',          // required; two-letter ISO language code
-        map_type: 'osm:standard',          // required
-        map_as_image: false,       // required
-        // map_subdomains: string,    // optional
-        slides: [{
-          type: "overview",      // optional; if present must be set to "overview"
-          // location: {            // required for all slides except "overview" slide
-          //   lat: decimal,      // latitude of point on map
-          //   lon: decimal       // longitude of point on map
-          // },
-          text: {                // optional if media present
-            headline: 'TEST',
-            text: 'TEST'       // may contain HTML markup
-          },
-          media: {               // optional if text present
-            url: 'https://miro.medium.com/max/10368/1*1XPSskRR5Mbe3X01BmV3zw.jpeg',       // url for featured media
-            // caption: string,   // optional; brief explanation of media content
-            // credit: string     // optional; creator of media content
-          }
-        }]           // required; array of slide objects (see below)
-      }
-    };
-
-    // var storymap = new KLStoryMap.StoryMap('mapdiv', storymap_data, storymap_options);
-    // console.debug('call');
-    // window.onresize = function (event) {
-    //   storymap.updateDisplay(); // this isn't automatic
-    // }
+    //   });
 
   }
 }
