97244cb7b5bfdc4dfbb3cbc2184bead866f6ea03
Daniel Warren - BGS <danwar@bgs.ac.uk>
2021-09-02 15:22:05 +0100

    feat: imported @material/layout-grid

--- src/material.module.ts
+++ src/material.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
   ]
 })
 export class MaterialModule { }
