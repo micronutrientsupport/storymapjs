ae4ddc54fc5c88c0a72bb09d3cb80789adf05075
Svea <svea@bgs.ac.uk>
2021-09-02 09:40:16 +0100

    refactor: storymap chnages, swapped images, text & locations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,7 +61,7 @@ function triggerStory() {
       language: 'en',          // required; two-letter ISO language code
       map_type: 'zoomify',           // required
       map_as_image: true,         // required; omit connecting lines between slide locations
-      map_background_color:  '#808080', // optional; hexadecimal color value for map background
+      map_background_color:  'white', // optional; hexadecimal color value for map background
       zoomify: {
         path: 'https://sveouu.github.io/MAPS_story/', // required; URL path to zoomable image folder
     //    path: './assets/images/',
@@ -74,10 +74,10 @@ function triggerStory() {
         location: {            // required for all slides except "overview" slide
         //   lat: decimal,      // latitude of point on map
         //   lon: decimal       // longitude of point on map
-        zoom: 10
+        zoom: 15
         },
         text: {                // optional if media present
-          headline: 'Some Castles',
+          headline: 'How the tool works',
           text: 'TEXT' + '<br>' + '<button id="test-button" >test</button>'       // may contain HTML markup
         },
         media: {               // optional if text present
@@ -88,48 +88,103 @@ function triggerStory() {
       },
       {
         location: {            // required for all slides except "overview" slide
-          lat: 80.9486,      // latitude of point on map
-          lon: -125.1999,       // longitude of point on map
+          lat: 83.9486,      // latitude of point on map
+          lon: -158,       // longitude of point on map
           zoom: 8
         },
         text: {                // optional if media present
-          headline: 'Edinburgh Castle',
-          text: 'Edinburgh' + '<br>' + '<button id="test-button" >test</button>'       // may contain HTML markup
+          headline: 'Country selection',
+          text: 'Country selection'
+           + '<br>' + '<button id="test-button" >Data Availability per country</button>'       // may contain HTML markup
         },
         media: {               // optional if text present
-          url: 'https://miro.medium.com/max/10368/1*1XPSskRR5Mbe3X01BmV3zw.jpeg',       // url for featured media
+          url: 'https://github.com/Sveouu/MAPS_story/blob/main/africa_map_selection.PNG?raw=true',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+      {
+        location: {            // required for all slides except "overview" slide
+          lat: 81,      // latitude of point on map
+          lon: -160
+          ,       // longitude of point on map
+          zoom: 8
+        },
+        text: {                // optional if media present
+          headline: 'Micronutrient selection',
+          text: 'The current estimate of the micronutrient situation in the population can be explored through: proxies for dietary composition [->], or through direct measures using biomarkers [->] pre-loaded in the tool, or provided by users [->]'
+           + '<br>' + '<button id="test-button" >test</button>'       // may contain HTML markup
+        },
+        media: {               // optional if text present
+          url: '',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+      {
+        location: {            // required for all slides except "overview" slide
+          lat: 81.5,      // latitude of point on map
+          lon: -50
+          ,       // longitude of point on map
+          zoom: 4
+        },
+        text: {                // optional if media present
+          headline: 'Baseline Map',
+          text: 'Present day micronutrient information for selection'
+           + '<br>' + '<button id="test-button" background-color: $color_primary >test</button>'       // may contain HTML markup
+        },
+        media: {               // optional if text present
+          url: 'https://github.com/Sveouu/MAPS_story/blob/main/malawi_map.PNG?raw=true',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+      {
+        location: {            // required for all slides except "overview" slide
+          lat: 81,      // latitude of point on map
+          lon: -10
+          ,       // longitude of point on map
+          zoom: 4
+        },
+        text: {                // optional if media present
+          headline: 'Baseline Graphs',
+          text: 'The current estimate of the micronutrient situation in the population can be explored through: proxies for dietary composition [->], or through direct measures using biomarkers [->] pre-loaded in the tool, or provided by users [->]'
+           + '<br>' + '<button id="test-button" >test</button>'       // may contain HTML markup
+        },
+        media: {               // optional if text present
+          url: 'https://github.com/Sveouu/MAPS_story/blob/main/MAPS_baseline_Graphs.PNG?raw=true',       // url for featured media
           // caption: string,   // optional; brief explanation of media content
           // credit: string     // optional; creator of media content
         }
       },
        {
           location: {            // required for all slides except "overview" slide
-            lat: 65.9486,      // latitude of point on map
-            lon: -125.1999,       // longitude of point on map
-            zoom: 6
+            lat: 76.5,      // latitude of point on map
+            lon: -130.1999,       // longitude of point on map
+            zoom: 4
           },
           text: {                // optional if media present
-            headline: 'Old Kincardine Castle',
-            text: 'Kincardine Estate, Auchterarder'       // may contain HTML markup
+            headline: 'Projections',
+            text: '...'       // may contain HTML markup
           },
           media: {               // optional if text present
-            url: 'https://i.pinimg.com/736x/d7/00/9d/d7009de6041addff3c9bf88c7c1bc147.jpg',       // url for featured media
+            url: 'https://github.com/Sveouu/MAPS_story/blob/main/MAPS_Malawi%20Projections.PNG?raw=true',       // url for featured media
             // caption: string,   // optional; brief explanation of media content
             // credit: string     // optional; creator of media content
           }
         },
         {
           location: {            // required for all slides except "overview" slide
-            lat: 65.9486,      // latitude of point on map
-            lon: -3.1999,       // longitude of point on map
-          zoom: 16
+            lat: 76.5,      // latitude of point on map
+            lon: -18.1999,       // longitude of point on map
+          zoom: 4
         },
           text: {                // optional if media present
-            headline: 'Drumlanrig Castle',
-            text: 'South of Glasgow'       // may contain HTML markup
+            headline: 'Dietary Change scenarios',
+            text: '...'       // may contain HTML markup
           },
           media: {               // optional if text present
-            url: 'https://www.undiscoveredscotland.co.uk/thornhill/drumlanrigcastle/images/drumlanrig-450.jpg',       // url for featured media
+            url: '',       // url for featured media
             // caption: string,   // optional; brief explanation of media content
             // credit: string     // optional; creator of media content
           }
