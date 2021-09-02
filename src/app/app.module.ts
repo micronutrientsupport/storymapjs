2b2c4eb5b36c18c79c56f8d7bf488220f8090925
Daniel Warren - BGS <danwar@bgs.ac.uk>
2021-09-02 12:31:34 +0100

    feat: routing scaffolding all working as expected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,38 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
-import { MatToolbarModule} from '@angular/material/toolbar';
-import { StoryOneComponent } from './pages/story-one/story-one.component';
-import { StoryTwoComponent } from './pages/story-two/story-two.component';
-import { StoryThreeComponent } from './pages/story-three/story-three.component';
-import { StoryFourComponent } from './pages/story-four/story-four.component';
-import { StoryFiveComponent } from './pages/story-five/story-five.component';
-import { StorySixComponent } from './pages/story-six/story-six.component';
-import { StorySevenComponent } from './pages/story-seven/story-seven.component';
-import { StoryEightComponent } from './pages/story-eight/story-eight.component';
-import { StoryNineComponent } from './pages/story-nine/story-nine.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    StoryOneComponent,
-    StoryTwoComponent,
-    StoryThreeComponent,
-    StoryFourComponent,
-    StoryFiveComponent,
-    StorySixComponent,
-    StorySevenComponent,
-    StoryEightComponent,
-    StoryNineComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoryOneComponent } from './story-one/story-one.component';
+import { StoryTwoComponent } from './story-two/story-two.component';
+import { StoryThreeComponent } from './story-three/story-three.component';
+import { StoryFourComponent } from './story-four/story-four.component';
+import { StoryFiveComponent } from './story-five/story-five.component';
+import { StorySixComponent } from './story-six/story-six.component';
+import { StoryEightComponent } from './story-eight/story-eight.component';
+import { StoryNineComponent } from './story-nine/story-nine.component';
+import { StorySevenComponent } from './story-seven/story-seven.component';
+
+
+
+@NgModule({
+  declarations: [
+    StoryOneComponent,
+    StoryTwoComponent,
+    StoryThreeComponent,
+    StoryFourComponent,
+    StoryFiveComponent,
+    StorySixComponent,
+    StorySevenComponent,
+    StoryEightComponent,
+    StoryNineComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
