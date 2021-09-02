2199a0d5a1277fe9b847e6c33ea1ca0f0b894cc8
Daniel Warren - BGS <danwar@bgs.ac.uk>
2021-09-02 14:06:59 +0100

    feat: created navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from 'src/material.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
