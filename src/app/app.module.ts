0c5b17f428b04d4f3e56f60b73256b9c4c82819e
Svea <svea@bgs.ac.uk>
2021-09-02 11:29:33 +0100

    chore: scss colours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LandingComponent } from './landing/landing.component';
 import { MatToolbarModule} from '@angular/material/toolbar';
+import { Route1Component } from './route1/route1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    Route1Component
   ],
   imports: [
     BrowserModule,
--- src/app/route1/route1.component.ts
+++ src/app/route1/route1.component.ts
@@ -1,54 +1,11 @@
 
-import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
+// declare function hello(): any;
 declare const KLStoryMap: any;
 
 function triggerStory() {
-
-  // var storymap_options = {
-  //   width: 600,                // required for embed tool; width of StoryMap
-  //   height: 600,               // required for embed tool; height of StoryMap
-  //   font_css: 'Calibri',              // optional; font set
-  //   calculate_zoom: true,              // optional; defaults to true.
-  //   storymap: {
-  //     language: 'en',          // required; two-letter ISO language code
-  //     map_type: 'osm:standard',          // required
-  //     map_as_image: true,       // required
-  //     // map_subdomains: string,    // optional
-  //     slides: [{
-  //       type: "overview",      // optional; if present must be set to "overview"
-  //       // location: {            // required for all slides except "overview" slide
-  //       //   lat: decimal,      // latitude of point on map
-  //       //   lon: decimal       // longitude of point on map
-  //       // },
-  //       text: {                // optional if media present
-  //         headline: 'TEST',
-  //         text: 'TEST'       // may contain HTML markup
-  //       },
-  //       media: {               // optional if text present
-  //         url: 'https://miro.medium.com/max/10368/1*1XPSskRR5Mbe3X01BmV3zw.jpeg',       // url for featured media
-  //         // caption: string,   // optional; brief explanation of media content
-  //         // credit: string     // optional; creator of media content
-  //       }
-  //     },
-  //       {
-  //         // type: "overview",      // optional; if present must be set to "overview"
-  //         location: {            // required for all slides except "overview" slide
-  //           lat: 55.9486,      // latitude of point on map
-  //           lon: -3.1999       // longitude of point on map
-  //         },
-  //         text: {                // optional if media present
-  //           headline: 'TEST',
-  //           text: 'TEST'       // may contain HTML markup
-  //         },
-  //         media: {               // optional if text present
-  //           url: 'https://miro.medium.com/max/10368/1*1XPSskRR5Mbe3X01BmV3zw.jpeg',       // url for featured media
-  //           // caption: string,   // optional; brief explanation of media content
-  //           // credit: string     // optional; creator of media content
-  //         }
-  //       }]           // required; array of slide objects (see below)
-  //   }
-  // };
+  console.debug('call');
 
 
   var storymap_options = {
@@ -200,12 +157,11 @@ function triggerStory() {
   }
 }
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-route1',
+  templateUrl: './route1.component.html',
+  styleUrls: ['./route1.component.scss']
 })
-
-export class AppComponent implements OnInit, AfterViewInit {
+export class Route1Component implements OnInit, AfterViewInit {
 
   // storymap_data can be an URL or a Javascript object
   public title = 'storymap';
@@ -215,6 +171,8 @@ export class AppComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     triggerStory();
+    // this.bla.addEventListener("click", () => { this.testClick(); });
+
   }
 
   ngAfterViewInit() {
@@ -227,3 +185,5 @@ export class AppComponent implements OnInit, AfterViewInit {
 
 
 }
+
+
