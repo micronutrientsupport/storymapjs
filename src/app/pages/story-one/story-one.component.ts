1e5dec566cd9a706fd5672c52fcb57a2978315c3
Svea <svea@bgs.ac.uk>
2021-09-03 08:23:22 +0100

    feat: story2 edited

--- src/app/pages/story-one/story-one.component.ts
+++ src/app/pages/story-one/story-one.component.ts
@@ -1,15 +1,245 @@
-import { Component, OnInit } from '@angular/core';
 
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
+
+declare const KLStoryMap: any;
+
+function triggerStory() {
+
+  // var storymap_options = {
+  //   width: 600,                // required for embed tool; width of StoryMap
+  //   height: 600,               // required for embed tool; height of StoryMap
+  //   font_css: 'Calibri',              // optional; font set
+  //   calculate_zoom: true,              // optional; defaults to true.
+  //   storymap: {
+  //     language: 'en',          // required; two-letter ISO language code
+  //     map_type: 'osm:standard',          // required
+  //     map_as_image: true,       // required
+  //     // map_subdomains: string,    // optional
+  //     slides: [{
+  //       type: "overview",      // optional; if present must be set to "overview"
+  //       // location: {            // required for all slides except "overview" slide
+  //       //   lat: decimal,      // latitude of point on map
+  //       //   lon: decimal       // longitude of point on map
+  //       // },
+  //       text: {                // optional if media present
+  //         headline: 'TEST',
+  //         text: 'TEST'       // may contain HTML markup
+  //       },
+  //       media: {               // optional if text present
+  //         url: 'https://miro.medium.com/max/10368/1*1XPSskRR5Mbe3X01BmV3zw.jpeg',       // url for featured media
+  //         // caption: string,   // optional; brief explanation of media content
+  //         // credit: string     // optional; creator of media content
+  //       }
+  //     },
+  //       {
+  //         // type: "overview",      // optional; if present must be set to "overview"
+  //         location: {            // required for all slides except "overview" slide
+  //           lat: 55.9486,      // latitude of point on map
+  //           lon: -3.1999       // longitude of point on map
+  //         },
+  //         text: {                // optional if media present
+  //           headline: 'TEST',
+  //           text: 'TEST'       // may contain HTML markup
+  //         },
+  //         media: {               // optional if text present
+  //           url: 'https://miro.medium.com/max/10368/1*1XPSskRR5Mbe3X01BmV3zw.jpeg',       // url for featured media
+  //           // caption: string,   // optional; brief explanation of media content
+  //           // credit: string     // optional; creator of media content
+  //         }
+  //       }]           // required; array of slide objects (see below)
+  //   }
+  // };
+
+
+  var storymap_options = {
+    width: 10000,                // required for embed tool; width of StoryMap
+    height: 4532,               // required for embed tool; height of StoryMap
+    font_css: 'Calibri',              // optional; font set
+    calculate_zoom: false,              // optional; defaults to true.
+    zoomControl: true,
+    storymap: {
+      language: 'en',          // required; two-letter ISO language code
+      map_type: 'zoomify',           // required
+      map_as_image: true,         // required; omit connecting lines between slide locations
+      map_background_color:  'white', // optional; hexadecimal color value for map background
+      zoomify: {
+        path: 'https://micronutrientsupport.github.io/MAPS_story/', // required; URL path to zoomable image folder
+    //    path: './assets/images/',
+        width: 5000,            // required; maximum width of image
+        height: 2266,           // required; maximum height of image
+        tolerance: 0.9         // required; display tolerance
+    },
+      slides: [{
+        type: "overview",      // optional; if present must be set to "overview"
+        location: {            // required for all slides except "overview" slide
+        //   lat: decimal,      // latitude of point on map
+        //   lon: decimal       // longitude of point on map
+        zoom: 15
+        },
+        text: {                // optional if media present
+
+          headline: 'How the tool works',
+          text: 'TEXT' + '<br>' + '<button id="test-button" >test</button>'       // may contain HTML markup
+        },
+        media: {               // optional if text present
+          url: '',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+      {
+        location: {            // required for all slides except "overview" slide
+          lat: 83.9486,      // latitude of point on map
+          lon: -158,       // longitude of point on map
+          zoom: 8
+        },
+        text: {                // optional if media present
+          headline: 'Country selection',
+          text: 'Country selection'
+           + '<br>' + '<button id="test-button" >Data Availability per country</button>'       // may contain HTML markup
+        },
+        media: {               // optional if text present
+          url: './assets/images/africa_map_selection.PNG',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+      {
+        location: {            // required for all slides except "overview" slide
+          lat: 81,      // latitude of point on map
+          lon: -160
+          ,       // longitude of point on map
+          zoom: 8
+        },
+        text: {                // optional if media present
+          headline: 'Micronutrient selection',
+          text: 'The current estimate of the micronutrient situation in the population can be explored through: proxies for dietary composition [->], or through direct measures using biomarkers [->] pre-loaded in the tool, or provided by users [->]'
+           + '<br>' + '<button id="test-button" >test</button>'       // may contain HTML markup
+        },
+        media: {               // optional if text present
+          url: '',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+      {
+        location: {            // required for all slides except "overview" slide
+          lat: 81.5,      // latitude of point on map
+          lon: -50
+          ,       // longitude of point on map
+          zoom: 4
+        },
+        text: {                // optional if media present
+          headline: 'Baseline Map',
+          text: 'Present day micronutrient information for selection'
+           + '<br>' + '<button class="angular-lookalike" id="test-button" >test</button>'       // may contain HTML markup
+        },
+        media: {               // optional if text present
+          url: './assets/images/malawi_map.PNG',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+      {
+        location: {            // required for all slides except "overview" slide
+          lat: 81,      // latitude of point on map
+          lon: -10
+          ,       // longitude of point on map
+          zoom: 4
+        },
+        text: {                // optional if media present
+          headline: 'Baseline Graphs',
+          text: 'The current estimate of the micronutrient situation in the population can be explored through: proxies for dietary composition [->], or through direct measures using biomarkers [->] pre-loaded in the tool, or provided by users [->]'
+           + '<br>' + '<button id="test-button" >test</button>'       // may contain HTML markup
+        },
+        media: {               // optional if text present
+          url: './assets/images/MAPS_baseline_Graphs.PNG',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+       {
+          location: {            // required for all slides except "overview" slide
+            lat: 76.5,      // latitude of point on map
+            lon: -130.1999,       // longitude of point on map
+            zoom: 4
+          },
+          text: {                // optional if media present
+            headline: 'Projections',
+            text: '...'       // may contain HTML markup
+          },
+          media: {               // optional if text present
+            url: './assets/images/MAPS_Malawi%20Projections.PNG',       // url for featured media
+            // caption: string,   // optional; brief explanation of media content
+            // credit: string     // optional; creator of media content
+          }
+        },
+        {
+          location: {            // required for all slides except "overview" slide
+            lat: 72.5,      // latitude of point on map
+            lon: -30.1999,       // longitude of point on map
+          zoom: 4
+        },
+          text: {                // optional if media present
+            headline: 'Dietary Change scenarios',
+            text: '...'       // may contain HTML markup
+          },
+          media: {               // optional if text present
+            url: '',       // url for featured media
+            // caption: string,   // optional; brief explanation of media content
+            // credit: string     // optional; creator of media content
+          }
+        },
+        {
+          location: {            // required for all slides except "overview" slide
+            lat: 76.5,      // latitude of point on map
+            lon: -120.1999,       // longitude of point on map
+          // zoom: 20
+        },
+          text: {                // optional if media present
+            headline: 'Video Walk through',
+            text: '...'       // may contain HTML markup
+          },
+          media: {               // optional if text present
+            url: '',       // url for featured media
+            // caption: string,   // optional; brief explanation of media content
+            // credit: string     // optional; creator of media content
+          }
+        }
+      ]           // required; array of slide objects (see below)
+    }
+  };
+
+  const storymap = new KLStoryMap.StoryMap('mapdiv', storymap_options);
+  window.onresize = function () {
+    storymap.updateDisplay(); // this isn't automatic
+  }
+}
 @Component({
   selector: 'app-story-one',
   templateUrl: './story-one.component.html',
   styleUrls: ['./story-one.component.scss']
 })
-export class StoryOneComponent implements OnInit {
 
-  constructor() { }
+export class StoryOneComponent implements OnInit, AfterViewInit {
 
-  ngOnInit(): void {
+  // storymap_data can be an URL or a Javascript object
+  public title = 'storymap';
+  public bla!: ElementRef;
+
+
+
+  ngOnInit() {
+    triggerStory();
   }
+
+  ngAfterViewInit() {
+    document.getElementById("test-button")?.addEventListener("click", () => { this.testClick(); });
+  }
+
+  public testClick() {
+    alert('click');
+  }
+
 
 }
--- src/app/pages/story-two/story-two.component.ts
+++ src/app/pages/story-two/story-two.component.ts
@@ -1,5 +1,109 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
+declare const KLStoryMap: any;
+
+function triggerStory() {
+  var storymap_options = {
+    width: 5000,                // required for embed tool; width of StoryMap
+    height: 4532,               // required for embed tool; height of StoryMap
+    font_css: 'Calibri',              // optional; font set
+    calculate_zoom: false,              // optional; defaults to true.
+    zoomControl: true,
+    storymap: {
+      language: 'en',          // required; two-letter ISO language code
+      map_type: 'zoomify',           // required
+      map_as_image: true,         // required; omit connecting lines between slide locations
+      map_background_color:  'white', // optional; hexadecimal color value for map background
+      zoomify: {
+        path: 'https://micronutrientsupport.github.io/MAPS_story2/', // required; URL path to zoomable image folder
+    //    path: './assets/images/',
+        width: 5000,            // required; maximum width of image
+        height: 3866,           // required; maximum height of image
+        tolerance: 0.9         // required; display tolerance
+    },
+      slides: [{
+        type: "overview",      // optional; if present must be set to "overview"
+        location: {            // required for all slides except "overview" slide
+
+        //   lat: decimal,      // latitude of point on map
+        //   lon: decimal       // longitude of point on map
+        zoom: 2
+        },
+        text: {                // optional if media present
+
+          headline: 'How does the MAPS tool make subnational estimates of dietary supply of micronutrients?',
+          text: 'TEXT' + '<br>' + '<button id="test-button" >test</button>'       // may contain HTML markup
+        },
+
+        media: {               // optional if text present
+          url: '',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+      {
+        location: {            // required for all slides except "overview" slide
+          lat: 60.9486,      // latitude of point on map
+          lon: -45,       // longitude of point on map
+          zoom: 3
+        },
+        text: {                // optional if media present
+          headline: 'Survey nutrition recall data',
+          text:
+
+           'The MAPS Tool combines data where participants recall the food they have consumed, with information on the nutrient composition of the food item.' + '<br>' +          'The MAPS Tool makes particular use of information from Living Standards Measurement Surveys – these surveys are undertaken relatively frequently, at a density that allows data to be presented broken down into categories such as the geographic region participants are from, what their relative wealth is or what time of year they took part in the survey.  This makes for a potential rich data source to inform our understanding of diet at the household level. However, there are assumptions which must be made using these data (as they are collected primarily to understand economic conditions) and limits to the extent to which we can use them. '+ '<button id="test-button" >Pros & Cons of LSMS Data DIALOG</button>' + '<br>' + '<button id="test-button" >Link to video walkthrough</button>',
+          },
+        media: {               // optional if text present
+          url: 'https://www.mydiasporakitchen.com/wp-content/uploads/2017/08/img_8989.jpg',       // url for featured media
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+           {
+        location: {            // Zoom in on Mallai map
+          lat: 60.9486,      // latitude of point on map
+          lon: -45,       // longitude of point on map
+          zoom: 3
+        },
+        text: {                // optional if media present
+          headline: 'Why use these data?  ',
+          text:
+
+           'Where survey data exist for a nation these can provide a nationally representative estimate of dietary supply.' + '<br>'+ 'The detail possible in the results will depend on the detail in the input data. ' +'<br>'+ 'These data provide a useful overview of the food system in a nation or region, and they can form an important component of the nutrition toolkit, or design of more detailed micronutrient surveys. ',
+          },
+        media: {               // media of datasource
+          url: './assets/images/Malawi_DataSource.PNG',       // url for featured media  './assets/images/malawi_map.PNG',
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      },
+      //https://selfhelpafrica.org/ie/wp-content/uploads/sites/4/2020/11/reports.jpg
+      {
+        location: {            // Zoom in on Mallai map
+          lat: 60.9486,      // latitude of point on map
+          lon: -45,       // longitude of point on map
+          zoom: 3
+        },
+        text: {                // optional if media present
+          headline: 'Data & Methods  ',
+          text:
+            'Data used:' + '<br>' + '✓ Malawi FCT with ...' + '<br>' +'✓ IHS4 LSMS data (2015-2016)' +'<p>&nbsp;</p>' + 'Data joined using food dictionary (LINK)' + '<br>' + 'See methods for full working methodology [LINK]'  +'<p>&nbsp;</p>' + 'Key points on the IHS4 data:  ' + '<br>' +  '➤ Data coverage is both nationally and subnationally representative for households  ' + '<br>' +  '➤ Data collection was conducted throughout a 13 month period, allowing seasonal comparison'+ '<br>' +  '➤ Data collection also includes (1) wealth quintiles (2) rural/urban locations'
+         },
+        media: {               // media of datasource
+          url: 'https://selfhelpafrica.org/ie/wp-content/uploads/sites/4/2020/11/reports.jpg',       // url for featured media  './assets/images/malawi_map.PNG',
+          // caption: string,   // optional; brief explanation of media content
+          // credit: string     // optional; creator of media content
+        }
+      }
+      ]           // required; array of slide objects (see below)
+    }
+  };
+
+  const storymap = new KLStoryMap.StoryMap('mapdiv', storymap_options);
+  window.onresize = function () {
+    storymap.updateDisplay(); // this isn't automatic
+  }
+}
 @Component({
   selector: 'app-story-two',
   templateUrl: './story-two.component.html',
@@ -8,8 +112,21 @@ import { Component, OnInit } from '@angular/core';
 export class StoryTwoComponent implements OnInit {
 
   constructor() { }
+  // storymap_data can be an URL or a Javascript object
+  public title = 'storymap';
+
 
   ngOnInit(): void {
+    triggerStory();
   }
+
+  ngAfterViewInit() {
+    document.getElementById("test-button")?.addEventListener("click", () => { this.testClick(); });
+  }
+
+  public testClick() {
+    alert('click');
+  }
+
 
 }
